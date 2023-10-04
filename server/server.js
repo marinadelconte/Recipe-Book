97c637e978d615325d840ed40b0d1faaea931967
Marina Del Conte <marinadelconte@yahoo.com>
2023-10-04 11:39:55 -0400

    added api info to server.js on backend

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require ('dotenv').config();
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
@@ -13,6 +14,14 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
+
+app.get("/searchRecipes/:searchTerm", async function (req, res) {
+const response = await fetch (`https://api.edamam.com/api/recipes/v2?type=public&q=${req.params.searchTerm}&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`)
+const data = await response.json()
+res.send(data)
+console.log(process.env.APP_ID)
+console.log(process.env.APP_KEY)
+} ) 
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
