701fd80782933e7ddd3773f0cf249c13b429ff03
Khalid G <kgibson4795@gmail.com>
2023-10-06 10:12:17 -0400

    change some bulma features

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -23,17 +23,17 @@ const AppNavbar = () => {
   return (
     <>
       
-      <Navbar.Item href="#" className=" pt-5 bg-dark pb-5" height="200">
+      <Navbar className=" pt-5 bg-dark pb-5" height="200">
         
-        <Navbar.Item as={Link} href="/" className="text-light is-size-3 mx-5">Home</Navbar.Item>
+        <Navbar.Item as={Link} href="/" className="text-light is-size-3 px-6" style={{textDecoration: 'none'}}>Home</Navbar.Item>
 
-        <Navbar.Item as={Link} href="/savedRecipes" className="text-light is-size-5 mx-5">My Saved Recipe Book</Navbar.Item>
+        <Navbar.Item as={Link} href="/savedRecipes" className="text-light is-size-5 px-6" style={{textDecoration: 'none'}}>My Saved Recipe Book</Navbar.Item>
 
         
-          <Navbar.Item as={Link}  href="/login" className="text-light is-pulled-right mx-auto">Login</Navbar.Item>
-          <Navbar.Item as={Link}  href="/signup" className="text-light is-pulled-right mx-auto">Sign Up</Navbar.Item>
+          <Navbar.Item as={Link}  href="/login" className="text-light px-6 mx-auto" style={{textDecoration: 'none'}}>Login</Navbar.Item>
+          <Navbar.Item as={Link}  href="/signup" className="text-light px-6 mx-auto" style={{textDecoration: 'none'}}>Sign Up</Navbar.Item>
         
-      </Navbar.Item>
+      </Navbar>
       
     </>
   );
